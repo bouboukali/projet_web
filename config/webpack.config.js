2cc0eb26d2c0311c36a7b55d8b8fbbbc5294ab9f
Sollami Florian <florian.sollami@student.vinci.be>
2018-11-17 16:15:47 +0100

    Ajout plugins webpack
    Ajout des 2 plugins webpack qui vont nous permettre d'avoir accès aux variabes d'environnements (.env) dans le frontend (sera fait à la compilation) :
    - Dotenv
    - DefinePlugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,6 +3,9 @@ const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
+const Dotenv = require('dotenv-webpack');
+
+
 const outputDirectory = 'dist';
 const projectRoot = path.resolve(__dirname, '..');
 
@@ -59,9 +62,26 @@ function buildConfig(env, argv) {
       new ManifestPlugin({
         writeToFileEmit: true
       }),
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+
+      new Dotenv({
+
+        safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+        systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+        silent: true // hide any errors
+      }),
+
+      // The DefinePlugin allows you to create global constants which can be configured at compile time.
+      // Va permettre d'avoir accès aux variables d'environnements (.env) dans notre fontend 
+      // https://medium.com/@trekinbami/using-environment-variables-in-react-6b0a99d83cf5
+      new webpack.DefinePlugin({
+
+        'process.env.AUTH0_CLIENT_ID': JSON.stringify(process.env.AUTH0_CLIENT_ID),
+        'process.env.AUTH0_CLIENT_SECRET': JSON.stringify(process.env.AUTH0_CLIENT_SECRET),
+        'process.env.AUTH0_DOMAIN': JSON.stringify(process.env.AUTH0_DOMAIN)
+      })
     ]
   };
 }
- 
+
 module.exports = buildConfig;
