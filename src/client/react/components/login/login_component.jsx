57e565805a64dcfda72925c7d00fec119c93f2ce
nawfal boujtat <nawfal.boujtat@student.vinci.be>
2018-11-29 22:28:21 +0100

    CSS du login component

--- src/client/react/components/login/login_component.jsx
+++ src/client/react/components/login/login_component.jsx
@@ -8,9 +8,12 @@ const LoginComponent = ({ email, password, authenticate, onFieldChange, login,lo
         <Container style={{alignItems: 'center',
         justifyContent: 'center'}}  >
       
+      <div class="col-lg-10">
+					<h3 style={{position: "fixed", left: "31%", top:"25%"}}>Connection with Auth0 Passwordless</h3>
+				</div>
             
-                <Button  type="submit" onClick={login}> Se connecter grâce à Auth0 Passwordless</Button>
-                <Button  type="submit" onClick={login2}> Email</Button>
+      <Button style={{position: "fixed", left: "30%", top:"40%"}} type="submit" onClick={login}> Using SMS</Button>
+        <Button style={{position: "fixed", left: "55%", top:"40%"}} type="submit" onClick={login2}> Using Email</Button>
         </Container>
     );
 };
